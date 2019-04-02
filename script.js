5ce167193cc53db981724f0a506f43dab870fd65
nellyhsu123 <33136548+nellyhsu123@users.noreply.github.com>
2019-04-02 15:57:24 -0700

    updated script file

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//console.log('Hello World!!!');
+console.log('Hello World!!!');
 
 /*********************
 NOTES
@@ -9,14 +9,13 @@ can't use keywords to name variables
 //Type coercion
 /*var firstName = 'Michael';
 var age = 35;
+
 console.log(firstName + ' ' + age);
+
 var hobby = prompt('What is his favorite hobby?');
 console.log(firstName + ' favorite hobby is ' + hobby);
 */
-
-/**************************
-*Coding Challenge 1
-
+/*
 var mHeight = 1.76;
 var mWeight = 68;
 var jHeight = 1.56;
@@ -31,13 +30,11 @@ console.log("John's BMI: " + jBMI);
 var mHigherBMI = mBMI > jBMI;
 
 console.log("Is Mark's BMI higher than John's? " + mHigherBMI);
-
 */
-
 
 /*******************
 * If / else statements
-*/
+
 
 var firstName = 'John';
 var civilStatus = 'single';
@@ -47,4 +44,61 @@ if(civilStatus === 'married') {
 } else {
 	console.log(firstName + ' will hopefully marry soon :)');
 }
+*/
+
+/*******************
+* The Ternary Operator
+*/
+
+var firstName = 'John';
+var age = 22;
+
+age >= 18 ? console.log(firstName + ' drinks beer') : console.log(firstName + ' drinks juice') 
+
+
+//Switch statement
+
+var job = 'teacher';
+
+switch (job) {
+	case 'teacher':
+		console.log(firstName + ' teaches kids how to code.');
+		break;
+	case 'driver':
+		console.log(firstName + ' drives an uber.');
+		break;
+	default:
+		console.log(firstName + ' does something else.');
+}
+
+/*******************
+* Truthy and Falsy values and equlaity operators
+*/
+
+// falsy values: undefined, null, 0, '', NaN (Not a Number)
+// truthy values: NOT falsy values
+
+/**********************
+* Coding Challenge 2
+*/
+
+jScoreAvg = (100 + 100 + 100) / 3
+
+mScoreAvg = (120 + 89 + 95) / 3
+
+maryScoreAvg = (96 + 87 + 115) / 3
+
+if(jScoreAvg > mScoreAvg && jScoreAvg > maryScoreAvg) {
+	console.log('John\'s team wins with an average score of: ' + jScoreAvg);
+}  else if(mScoreAvg > jScoreAvg && mScoreAvg > maryScoreAvg){
+	console.log('Mike\'s team wins with an average score of: ' + mScoreAvg);
+} else if(maryScoreAvg > jScoreAvg && maryScoreAvg > mScoreAvg){
+	console.log('Marry\'s team wins with an average score of: ' + maryScoreAvg);
+} else {
+	console.log('It\'s a tie!');
+}
+
+
+
+
 
