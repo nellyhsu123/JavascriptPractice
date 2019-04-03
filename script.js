70c3ef83f32efe2290c40e241b6576d758837369
nellyhsu123 <33136548+nellyhsu123@users.noreply.github.com>
2019-04-02 20:23:48 -0700

    lesson 2 part 25

--- script.js
+++ script.js
@@ -9,9 +9,7 @@ can't use keywords to name variables
 //Type coercion
 /*var firstName = 'Michael';
 var age = 35;
-
 console.log(firstName + ' ' + age);
-
 var hobby = prompt('What is his favorite hobby?');
 console.log(firstName + ' favorite hobby is ' + hobby);
 */
@@ -20,37 +18,30 @@ var mHeight = 1.76;
 var mWeight = 68;
 var jHeight = 1.56;
 var jWeight = 70;
-
 var mBMI = mWeight / (mHeight * mHeight);
 var jBMI = jWeight / (jHeight * jHeight);
-
 console.log("Mark's BMI: " + mBMI);
 console.log("John's BMI: " + jBMI);
-
 var mHigherBMI = mBMI > jBMI;
-
 console.log("Is Mark's BMI higher than John's? " + mHigherBMI);
 */
 
 /*******************
 * If / else statements
-
-
 var firstName = 'John';
 var civilStatus = 'single';
-
 if(civilStatus === 'married') {
 	console.log(firstName + ' is married!');
 } else {
 	console.log(firstName + ' will hopefully marry soon :)');
 }
-*/
+
 
 /*******************
 * The Ternary Operator
 */
 
-var firstName = 'John';
+/*var firstName = 'John';
 var age = 22;
 
 age >= 18 ? console.log(firstName + ' drinks beer') : console.log(firstName + ' drinks juice') 
@@ -80,7 +71,6 @@ switch (job) {
 
 /**********************
 * Coding Challenge 2
-*/
 
 jScoreAvg = (100 + 100 + 100) / 3
 
@@ -99,6 +89,40 @@ if(jScoreAvg > mScoreAvg && jScoreAvg > maryScoreAvg) {
 }
 
 
+var john = ['John', 'Smith', 1990, 'designer'];
 
+var isDesigner = john.indexOf('designer') !== -1 ? 'John IS a designer' : 'John is NOT a designer';
+
+console.log(isDesigner);
+*/
+
+/*******************
+* Coding Challenge 3
+*/
+
+function tipCalculator(bill) {
+	if(bill < 50) {
+		return bill * 0.2;
+	} else if(bill > 50 && bill < 200) {
+		return bill * 0.15;
+	} else if(bill > 200) {
+		return bill * 0.1;
+	}
+}
+var bills = [124, 48, 268];
+var tips = [];
+var totals = [];
+
+tips.push(tipCalculator(bills[0]));
+tips.push(tipCalculator(bills[1]));
+tips.push(tipCalculator(bills[2]));
+
+console.log(tips);
+
+totals.push(bills[0] + tips[0]);
+totals.push(bills[1] + tips[1]);
+totals.push(bills[2] + tips[2]);
+
+console.log(totals);
 
 
